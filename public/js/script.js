dded11ec8c897b29cacc8afc5c95dc4c3eb7eea2
Aditya Yadav <Adityay1109@gmail.com>
2024-12-26 21:50:48 +0530

    .

--- public/js/script.js
+++ public/js/script.js
@@ -4,43 +4,43 @@ if (navigator.geolocation) {
   navigator.geolocation.watchPosition(
     (position) => {
       const { latitude, longitude } = position.coords;
+      console.log("Latitude:", latitude, "Longitude:", longitude); // Debugging log
       socket.emit("send location", { latitude, longitude });
     },
     (error) => {
       console.error(error);
-    },{
-        enableHighAccuracy:true,
-        maximumAge:0,
-        timeout:5000,
-
+    },
+    {
+      enableHighAccuracy: true,
+      maximumAge: 0,
+      timeout: 5000,
     }
   );
 }
 
+const map = L.map("map").setView([0, 0], 16);
 
-const map = L.map("map").setView([0,0],16)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Open Map",
+}).addTo(map);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution:"Open Map"
-}).addTo(map)
+const markers = {};
 
-const markers ={};
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  console.log("Received location for", id, "Lat:", latitude, "Lng:", longitude); // Debugging log
+  map.setView([latitude, longitude]);
 
-
-socket.on("receive-location",(data)=>{
-    const {id,latitude,longitude}=data
-    map.setView([latitude,longitude]);
-    if(markers[id]){
-        markers[id].setLatLng([latitude,longitude])
-    }else{
-        markers[id]=L.marker([latitude,longitude]).addTo(map)
-    }
-
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
 });
 
 socket.on("user-disconnected", (id) => {
-    if(markers[id]){
-        map.removeLayer(markers[id])
-        delete markers[id]
-    }
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
 });
